96a90fb42215769154d864571a9c4fdeabaa0043
Anurag Rajawat <anurag@stepsecurity.io>
2026-05-04 13:39:25 +0530

    feat: Onboard bazelbuild/setup-bazelisk action
    Signed-off-by: Anurag Rajawat <anurag@stepsecurity.io>

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
-# setup-bazelisk
+[![StepSecurity Maintained Action](https://raw.githubusercontent.com/step-security/maintained-actions-assets/main/assets/maintained-action-banner.png)](https://docs.stepsecurity.io/actions/stepsecurity-maintained-actions)
+
+# setup-bazelisk v3
+Set up your GitHub Actions workflow with a specific version of Bazelisk
+
+Note that GitHub Actions includes Bazelisk by default as of <https://github.com/actions/virtual-environments/pull/490> so this setup is not necessary unless you want to customize the Bazelisk version, or are running Bazel inside a container.
+
+This action sets up Bazelisk for use in actions by:
+
+- optionally downloading and caching a version of Bazelisk by version and adding to PATH
+- setting up cache for downloaded Bazel versions
+
+# Usage
+
+See [action.yml](action.yml)
+
+Basic:
+```yaml
+steps:
+- uses: actions/checkout@v6
+- uses: step-security/setup-bazelisk@v3
+- name: Mount bazel cache  # Optional
+  uses: actions/cache@v5
+  with:
+    path: "~/.cache/bazel"
+    key: bazel
+- run: bazel build //...
+```
+
+# Known issues on Windows
+* This action doesn't work with PowerShell. Make sure to have `shell: bash` in your `run:` steps.
+* Windows removes one of the slashes (`/`) when two are present (`bazel test //tests/...` becomes `bazel test /tests/...` and fails). 
+  As a workaround, don't have any prefix `//`. Since all runs start at WORKSPACE dir, it should work all the same.
+
+Full workaround example on windows:
+```yaml
+- name: Run tests
+  run: bazel test tests/...
+  shell: bash
+```
+
+# License
+
+The scripts and documentation in this project are released under the [MIT License](LICENSE)
---
+++ src/installer.ts
@@ -0,0 +1,165 @@
+import * as core from '@actions/core';
+import * as github from '@actions/github';
+import {GitHub} from '@actions/github/lib/utils';
+import {createUnauthenticatedAuth} from '@octokit/auth-unauthenticated';
+import * as semver from 'semver';
+import * as tc from '@actions/tool-cache';
+import fs from 'fs';
+import os from 'os';
+
+export interface IBazeliskAsset {
+  name: string;
+  browser_download_url: string;
+}
+
+export interface IBazeliskVersion {
+  tag_name: string;
+  draft: boolean;
+  prerelease: boolean;
+  assets: IBazeliskAsset[];
+}
+
+export async function getBazelisk(
+  versionSpec: string,
+  token: string
+): Promise<string> {
+  const toolPath: string = tc.find('bazelisk', versionSpec);
+
+  if (toolPath) {
+    core.info(`Found in cache @ ${toolPath}`);
+    return toolPath;
+  }
+
+  core.info(`Attempting to download ${versionSpec}...`);
+
+  // Possible values are 'aix', 'darwin', 'freebsd', 'linux', 'openbsd', 'sunos' and 'win32'.
+  // Bazelisk filenames use 'darwin', 'linux' and 'windows'.
+  let osPlatform: string = os.platform();
+  // Possible values are 'arm', 'arm64', 'ia32', 'mips', 'mipsel', 'ppc', 'ppc64', 's390', 's390x' and 'x64'.
+  // Bazelisk filenames use 'amd64' and 'arm64'.
+  let osArch: string = os.arch();
+  if (osArch == 'x64') {
+    osArch = 'amd64';
+  }
+  let osFileName: string = `bazelisk-${osPlatform}-${osArch}`;
+  if (osPlatform == 'win32') {
+    osFileName = `bazelisk-windows-${osArch}.exe`;
+  }
+
+  const info = await findMatch(versionSpec, osFileName, token);
+  if (!info) {
+    throw new Error(
+      `Unable to find Bazelisk version '${versionSpec}' for platform ${osPlatform} and arch ${osArch}.`
+    );
+  }
+  return await cacheBazelisk(info, osFileName, token);
+}
+
+async function cacheBazelisk(
+  info: IBazeliskVersion,
+  osFileName: string,
+  token: string
+): Promise<string> {
+  const downloadPrefix: string =
+    'https://github.com/bazelbuild/bazelisk/releases/download';
+  const downloadUrl: string = `${downloadPrefix}/${info.tag_name}/${osFileName}`;
+  core.info(`Acquiring ${info.tag_name} from ${downloadUrl}`);
+  const auth = `token ${token}`;
+  const downloadPath: string = await tc.downloadTool(
+    downloadUrl,
+    undefined,
+    auth
+  );
+
+  core.info('Adding to the cache ...');
+  fs.chmodSync(downloadPath, '755');
+  const cachePath: string = await tc.cacheFile(
+    downloadPath,
+    'bazel',
+    'bazelisk',
+    info.tag_name
+  );
+  core.info(`Successfully cached bazelisk to ${cachePath}`);
+  return cachePath;
+}
+
+async function findMatch(
+  versionSpec: string,
+  osFileName: string,
+  token: string
+): Promise<IBazeliskVersion | undefined> {
+  let versions = new Map<string, IBazeliskVersion>();
+  let bazeliskVersions = await getVersionsFromDist(token);
+
+  bazeliskVersions.forEach((bazeliskVersion: IBazeliskVersion) => {
+    const hasRelevantAsset: boolean = bazeliskVersion.assets.some(
+      (asset: IBazeliskAsset) => {
+        return asset.name == osFileName;
+      }
+    );
+    if (hasRelevantAsset) {
+      const version: semver.SemVer | null = semver.coerce(
+        bazeliskVersion.tag_name
+      );
+      if (version) {
+        versions.set(version.version, bazeliskVersion);
+      }
+    }
+  });
+
+  // get the latest version that matches the version spec
+  let version: string = evaluateVersions(
+    Array.from(versions.keys()),
+    versionSpec
+  );
+  return versions.get(version);
+}
+
+async function getVersionsFromDist(token: string): Promise<IBazeliskVersion[]> {
+  const isGHES = process.env.GITHUB_SERVER_URL !== 'https://github.com';
+
+  const octokit = isGHES
+    ? new GitHub({
+        baseUrl: 'https://api.github.com',
+        authStrategy: createUnauthenticatedAuth,
+        auth: {
+          reason: 'unauthenticated public API call from GHES runner'
+        }
+      })
+    : github.getOctokit(token);
+
+  const releases = await octokit.paginate(octokit.rest.repos.listReleases, {
+    owner: 'bazelbuild',
+    repo: 'bazelisk',
+    per_page: 100
+  });
+  return releases || [];
+}
+
+// Copied from @actions/tool-cache.
+function evaluateVersions(versions: string[], versionSpec: string): string {
+  let version = '';
+  core.debug(`evaluating ${versions.length} versions`);
+  versions = versions.sort((a, b) => {
+    if (semver.gt(a, b)) {
+      return 1;
+    }
+    return -1;
+  });
+  for (let i = versions.length - 1; i >= 0; i--) {
+    const potential: string = versions[i];
+    const satisfied: boolean = semver.satisfies(potential, versionSpec);
+    if (satisfied) {
+      version = potential;
+      break;
+    }
+  }
+
+  if (version) {
+    core.debug(`matched: ${version}`);
+  } else {
+    core.debug('match not found');
+  }
+
+  return version;
+}
